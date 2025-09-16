9ae5b29983ab09fb7e5f9b0600d198262d262e1f
zharkrin <zarkrin2@gmail.com>
2025-09-16 13:07:18 +0200

    Actualizar coloresTerreno.js

--- frontend/js/mapa/coloresTerreno.js
+++ frontend/js/mapa/coloresTerreno.js
@@ -1,10 +1,8 @@
-// frontend/js/mapa/coloresTerreno.js
-
 export const coloresTerreno = {
-    llanura: "#88c070",   // verde claro
-    montaña: "#8c8c8c",   // gris rocoso
-    desierto: "#e0c068",  // arena
-    bosque: "#228b22",    // verde intenso
-    helada: "#a9d0f5",    // azul pálido (tundra helada)
-    glaciar: "#ffffff"    // blanco puro (hielo y nieve)
+    llanura: "#88c070",
+    bosque: "#228B22",
+    desierto: "#EDC9Af",
+    montaña: "#A9A9A9",
+    helada: "#B0E0E6",
+    glaciar: "#E0FFFF"
 };
