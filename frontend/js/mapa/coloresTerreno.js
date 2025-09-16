d26120b448691e9d8f97561a3b6b4cd84393cac0
zharkrin <zarkrin2@gmail.com>
2025-09-16 09:08:46 +0200

    Create coloresTerreno.js

---
+++ frontend/js/mapa/coloresTerreno.js
@@ -0,0 +1,10 @@
+// frontend/js/mapa/coloresTerreno.js
+
+export const coloresTerreno = {
+    llanura: "#88c070",   // verde claro
+    montaña: "#8c8c8c",   // gris rocoso
+    desierto: "#e0c068",  // arena
+    bosque: "#228b22",    // verde intenso
+    helada: "#a9d0f5",    // azul pálido (tundra helada)
+    glaciar: "#ffffff"    // blanco puro (hielo y nieve)
+};
