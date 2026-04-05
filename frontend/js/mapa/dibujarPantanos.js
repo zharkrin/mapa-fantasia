5cb3d37e09c8b09c70da07d1f8d948020d178e40
zharkrin <zarkrin2@gmail.com>
2026-04-05 13:19:50 +0200

    Actualizar dibujarPantanos.js

--- frontend/js/mapa/dibujarPantanos.js
+++ frontend/js/mapa/dibujarPantanos.js
@@ -1,20 +1,26 @@
-/**************************************************
- * DIBUJAR PANTANOS
- **************************************************/
+// =======================================================
+// DIBUJAR PANTANOS 
+// =======================================================
 
-function dibujarPantanos(pantanos, mapaContainer, tamCelda) {
-  const anteriores = mapaContainer.querySelectorAll(".pantano");
-  anteriores.forEach(p => p.remove());
+import { gridAPixel } from "../utils/coordenadas.js";
 
-  pantanos.forEach(celda => {
-    const div = document.createElement("div");
-    div.className = "pantano";
-    div.style.left = `${celda.x * tamCelda}px`;
-    div.style.top = `${celda.y * tamCelda}px`;
-    div.style.width = `${tamCelda}px`;
-    div.style.height = `${tamCelda}px`;
-    mapaContainer.appendChild(div);
-  });
+export function dibujarPantanos(pantanos, mapaContainer, tamCelda) {
+
+    const anteriores = mapaContainer.querySelectorAll(".pantano");
+    anteriores.forEach(p => p.remove());
+
+    pantanos.forEach(celda => {
+
+        const { x, y } = gridAPixel(celda.x, celda.y, tamCelda);
+
+        const div = document.createElement("div");
+        div.className = "pantano";
+
+        div.style.left = `${x}px`;
+        div.style.top = `${y}px`;
+        div.style.width = `${tamCelda}px`;
+        div.style.height = `${tamCelda}px`;
+
+        mapaContainer.appendChild(div);
+    });
 }
-
-window.dibujarPantanos = dibujarPantanos;
